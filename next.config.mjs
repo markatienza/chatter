011f472cac65aecc3d2eb8f07953697f96c8b271
Mark Atienza <atienza.markangelo26@gmail.com>
2024-04-02 01:00:17 +0800

    fix: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        GOOGLE_AUTH_CLIENT_ID: process.env.GOOGLE_AUTH_CLIENT_ID,
-        GOOGLE_AUTH_CLIENT_SECRET: process.env.GOOGLE_AUTH_CLIENT_SECRET,
-    }
+  env: {
+    GOOGLE_AUTH_CLIENT_ID: process.env.GOOGLE_AUTH_CLIENT_ID,
+    GOOGLE_AUTH_CLIENT_SECRET: process.env.GOOGLE_AUTH_CLIENT_SECRET,
+    NEXT_PUBLIC_REACT_AUTH_APP_ID: process.env.NEXT_PUBLIC_REACT_AUTH_APP_ID,
+    NEXT_PUBLIC_GITHUB_CLIENT_ID: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
+    NEXT_PUBLIC_GITHUB_CLIENT_SECRET:process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
+  },
 };
 
 export default nextConfig;
