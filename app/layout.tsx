36c6439ee41e932dfb11d729ff1ea5b7e0133c90
Mark <atienza.markangelo26@gmail.com>
2024-04-06 23:05:21 +0800

    feat: add clerk for auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <StoreProvider>{children}</StoreProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <StoreProvider>{children}</StoreProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
